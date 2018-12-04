751409831efe10386e2b590ad84d68f31e6bdcf8
CristoferNC <cristofercorporan675@hotmail.com>
2018-12-04 01:29:42 -0400

    Creacion de nuestra vista

---
+++ sistema/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;Bienvenido a nuestro sistema&#8221;
+          <footer>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
